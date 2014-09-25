7cc82e7c71c3a3446e5cdb7c6c4a0c29b290ed04
LzStudio <LzStudioDota2@LzStudios-MacBook-Pro.local>
2014-09-25 11:19:08 +0700

    1st commit

---
+++ LzTalk/LzTalk/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LzTalk
+//
+//  Created by LzStudio on 9/25/14.
+//  Copyright (c) 2014 LTT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LzTalk/LzTalk/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LzTalk
+//
+//  Created by LzStudio on 9/25/14.
+//  Copyright (c) 2014 LTT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
