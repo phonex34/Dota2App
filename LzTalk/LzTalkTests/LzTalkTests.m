2f2c69a4b18339b3f90f87c7a20aee6a9e4a8e40
LzStudio <LzStudioDota2@LzStudios-MacBook-Pro.local>
2014-09-25 11:36:01 +0700

    2nd commit

---
+++ LzTalk/LzTalkTests/LzTalkTests.m
@@ -0,0 +1,34 @@
+//
+//  LzTalkTests.m
+//  LzTalkTests
+//
+//  Created by LzStudio on 9/25/14.
+//  Copyright (c) 2014 LTT. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LzTalkTests : XCTestCase
+
+@end
+
+@implementation LzTalkTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
